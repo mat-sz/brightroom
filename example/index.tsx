6776695b308ff47444c5dde4a6ba4cb4587740b6
Mat Sz <contact@matsz.dev>
2020-05-16 17:17:36 +0200

    added a button to test export

--- example/index.tsx
+++ example/index.tsx
@@ -1,7 +1,15 @@
 import Brightroom from '../.';
 import example from './example.jpg';
 
-new Brightroom({
+const brightroom = new Brightroom({
   image: example,
   container: document.getElementById('root')!
 });
+
+const exportBtn = document.getElementById('export');
+if (exportBtn) {
+  exportBtn.addEventListener('click', async () => {
+    const canvas: HTMLCanvasElement = await brightroom.toCanvas();
+    window.open(canvas.toDataURL(), '_blank');
+  });
+}
